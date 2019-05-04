b3e9d3ffc9222b4bbdad2cdd8f5d3f51d32e515d
kanin kearpimy <kanin23814@gmail.com>
2019-05-04 15:34:03 +0700

    first commit

---
+++ precache-manifest.f1bdb89e2c51bd7241ba003ee0a52518.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa60803c705198aa69c6d6f5c26999c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca066dcf9bb903d6dca7",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ca066dcf9bb903d6dca7",
+    "url": "/static/js/main.224ffbf7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
